eea3d270f912e4fe58db8df8542c94eeb63ee4fd
Gabriel Dakinah Vincent <gabrieldvincent0100@outlook.com>
2025-08-22 02:10:04 +0000

    enhance: color highlighting for options, errors, and success messages

--- extract-aliases/src/extract_aliases.sh
+++ extract-aliases/src/extract_aliases.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+# Colors
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+NC='\033[0m' # No Color
+
 # Function to display usage information
 usage() {
-    echo "Usage: $0 [-o output_file] [-q]"
-    echo "  -o output_file  Specify the output markdown file name (default: aliases.md)"
-    echo "  -q              Quiet mode, do not open the markdown file"
+    echo -e "${YELLOW}Usage:${NC} $0 [${GREEN}-o${NC} ${WHITE}output_file${NC}] [${GREEN}-q${NC}]"
+    echo -e "  ${GREEN}-o${NC} ${WHITE}output_file${NC}  Specify the output markdown file name (default: aliases.md)"
+    echo -e "  ${GREEN}-q${NC}              Quiet mode, do not open the markdown file"
     exit 1
 }
 
@@ -48,14 +59,14 @@ case "$default_shell" in
         config_file="$HOME/.kshrc"
         ;;
     *)
-        echo "Unsupported shell: $default_shell"
+        echo -e "${RED}Error:${NC} Unsupported shell: $default_shell"
         exit 1
         ;;
 esac
 
 # Check if the configuration file exists
 if [[ ! -f "$config_file" ]]; then
-    echo "Configuration file not found: $config_file"
+    echo -e "${RED}Error:${NC} ${YELLOW}Configuration file not found:${NC} $config_file"
     exit 1
 fi
 
@@ -74,7 +85,7 @@ done; then
     echo "Markdown file $output_file may be empty."
 fi
 
-echo "Aliases have been written to $output_file"
+echo -e "${GREEN}Aliases have been written to:${NC} $output_file"
 
 # Open the markdown file if not in quiet mode
 if [ "$quiet_mode" = false ]; then
