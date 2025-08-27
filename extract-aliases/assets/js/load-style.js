f6134fc49c225c8201df00614e17d131f1989ba7
Gabriel Dakinah Vincent <gabrieldvincent0100@outlook.com>
2025-08-27 14:00:02 +0000

    Move js and css folders under extract-aliases into assets folder and update references in related files

--- extract-aliases/assets/js/load-style.js
+++ extract-aliases/assets/js/load-style.js
@@ -2,6 +2,6 @@
 (function() {
     const link = document.createElement('link');
     link.rel = 'stylesheet';
-    link.href = "https://cdn.jsdelivr.net/gh/Gabriel-Dakinah-Vincent/Bash-exercises/extract-aliases/css/styles.css"; 
+    link.href = "https://cdn.jsdelivr.net/gh/Gabriel-Dakinah-Vincent/Bash-exercises/extract-aliases/assets/css/styles.css"; 
     document.head.appendChild(link);
 })();
